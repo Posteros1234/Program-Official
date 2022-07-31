1232bfa2423ec7a6f248e59793f83b0d24b8a948
Posteros1234 <109865261+Posteros1234@users.noreply.github.com>
2022-07-31 21:37:54 +0700

    Created Version 1
    Created Version 1

---
+++ build-Official_Release-Qt_6_3_1_for_iOS_Simulator/CMakeFiles/3.23.2/CompilerIdCXX/CompilerIdCXX.build/Debug-iphonesimulator/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/natanon/Documents/Qt/Program-Official/build-Official_Release-Qt_6_3_1_for_iOS_Simulator/CMakeFiles/3.23.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ build-Official_Release-Qt_6_3_1_for_iOS_Simulator/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Xcode" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/natanon/Qt/Qt Creator.app/Contents/Resources/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/natanon/Qt/6.3.1/ios/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/natanon/Qt/6.3.1/ios" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "/Users/natanon/Qt/6.3.1/ios/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_OSX_ARCHITECTURES" "x86_64" CACHE "STRING" "" FORCE)
+set("CMAKE_OSX_SYSROOT" "iphonesimulator" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
